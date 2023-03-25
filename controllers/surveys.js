a66a9ea478b88fb510dda4037e7d02daec9451e8
Bobby Escario <bobby@Bobbys-MBP.ht.home>
2023-03-25 13:34:15 -0400

    fix undefined error when deploying

--- controllers/surveys.js
+++ controllers/surveys.js
@@ -4,3 +4,27 @@ let router = express.Router();
 module.exports.displaySurveys = (req, res, next)=>{
 	res.render('surveys/index', {title: "Surveys"});
 }
+
+module.exports.displayAddSurvey = (req, res, next)=>{
+
+}
+
+module.exports.addSurvey = (req, res, next)=>{
+
+}
+
+module.exports.displayEditSurvey = (req, res, next)=>{
+
+}
+
+module.exports.editSurvey = (req, res, next)=>{
+
+} 
+
+module.exports.displaySurvey = (req, res, next)=>{
+
+}
+
+module.exports.destroySurvey = (req, res, next)=>{
+
+}
