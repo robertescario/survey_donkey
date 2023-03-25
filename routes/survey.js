efcbd056f9ff68eb5354ebf6f309790c69a1cded
Bobby Escario <bobby@Bobbys-MBP.ht.home>
2023-03-25 12:21:52 -0400

    updated the routes to use RESTful convention

--- routes/survey.js
+++ routes/survey.js
@@ -4,6 +4,19 @@ let mongoose = require('mongoose');
 let passport = require('passport');
 let surveyController = require('../controllers/surveys');
 
+// GET index page of survey
 router.get('/', surveyController.displaySurveys);
+// GET the ADD survey form
+router.get('/add', surveyContoller.displayAddSurvey);
+// POST the survey data to create record
+router.post('/add', surveyContoller.addSurvey);
+// GET the EDIT survey form
+router.get('/:id/edit', surveyController.displayEditSurvey);
+// POST the survey data to update record with id = :id
+router.post('/:id/edit', surveyController.editSurvey);
+// GET the page to show survey details
+router.get('/:id', surveyContoller.displaySurvey);
+// GET process the delete record by survey id
+router.get('/:id/delete', surveyController.destroySurvey);
 
 module.exports = router;
