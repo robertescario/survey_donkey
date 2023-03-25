646add73e54c1eea8947a8e56e8d044c07ee0b20
Bobby Escario <bobby@Bobbys-MBP.ht.home>
2023-03-25 13:27:43 -0400

    fix spelling errors

--- routes/survey.js
+++ routes/survey.js
@@ -7,15 +7,15 @@ let surveyController = require('../controllers/surveys');
 // GET index page of survey
 router.get('/', surveyController.displaySurveys);
 // GET the ADD survey form
-router.get('/add', surveyContoller.displayAddSurvey);
+router.get('/add', surveyController.displayAddSurvey);
 // POST the survey data to create record
-router.post('/add', surveyContoller.addSurvey);
+router.post('/add', surveyController.addSurvey);
 // GET the EDIT survey form
 router.get('/:id/edit', surveyController.displayEditSurvey);
 // POST the survey data to update record with id = :id
 router.post('/:id/edit', surveyController.editSurvey);
 // GET the page to show survey details
-router.get('/:id', surveyContoller.displaySurvey);
+router.get('/:id', surveyController.displaySurvey);
 // GET process the delete record by survey id
 router.get('/:id/delete', surveyController.destroySurvey);
 
