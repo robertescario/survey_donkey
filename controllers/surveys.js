df2c8fe78d409f2cc31db1516b02ced6357d9f12
Bobby Escario <bobby@Bobbys-MacBook-Pro.local>
2023-04-03 15:11:36 -0400

    allow access to index without login but restrict on controller level other access to survey

--- controllers/surveys.js
+++ controllers/surveys.js
@@ -2,98 +2,108 @@ let express = require('express');
 let router = express.Router();
 const Survey = require('../models/survey');
 
-module.exports.displaySurveys = (req, res, next) => {
-  
+module.exports.displaySurveys = (req, res, next) => { 
+  // find all surveys in the surveys collection
+  Survey.find((err, surveys) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('surveys/index', {
+        title: 'Surveys',
+        surveys: surveys,
+        displayName: req.user ? req.user.displayName : ''
+      });
+    }
+    
+  });
+};
+
+module.exports.displayAddSurvey = (req, res, next) => {
   if (!req.user) {
     res.redirect('/login');
 
   }
   else { 
-    // find all surveys in the surveys collection
-    
-    Survey.find((err, surveys) => {
-      if (err) {
-        return console.error(err);
-      }
-      else {
-        res.render('surveys/index', {
-          title: 'Surveys',
-          surveys: surveys,
-          displayName: req.user ? req.user.displayName : ''
-        });
-      }
-      
+    res.render('surveys/add', {
+      title: 'Add Survey',
+      surveys: {}, // create an empty surveys object
+      displayName: req.user ? req.user.displayName : ''
     });
   }
-
-
-};
-
-module.exports.displayAddSurvey = (req, res, next) => {
-  res.render('surveys/add', {
-    title: 'Add Survey',
-    surveys: {}, // create an empty surveys object
-    displayName: req.user ? req.user.displayName : ''
-  });
 };
 
 module.exports.addSurvey = (req, res, next) => {
-  // Get form data from request body
-  let newSurvey = Survey({
-    title: req.body.title,
-    description: req.body.description
-  });
+  if (!req.user) {
+    res.redirect('/login');
 
-  // Save the new survey to the database
-  newSurvey.save((err) => {
-    if (err) {
-      console.error(err);
-      res.end(err);
-    } else {
-      res.redirect('/surveys');
-    }
-  });
+  }
+  else {
+    // Get form data from request body
+    let newSurvey = Survey({
+      title: req.body.title,
+      description: req.body.description
+    });
+
+    // Save the new survey to the database
+    newSurvey.save((err) => {
+      if (err) {
+        console.error(err);
+        res.end(err);
+      } else {
+        res.redirect('/surveys');
+      }
+    });
+  }
 };
 
 module.exports.displayEditSurvey = (req, res, next) => {
-  let id = req.params.id;
-  Survey.findById(req.params.id, (err, survey) => {
-    if (err) {
-      console.error(err);
-      res.end(err);
-    } else {
-      res.render('surveys/edit', {
-        title: survey.title,
-        description: survey.description,
-        survey: survey,
-        displayName: req.user ? req.user.displayName : ''
-      });
-    }
-  });
+  if (!req.user) {
+    res.redirect('/login');
 
+  }
+  else {
+    let id = req.params.id;
+    Survey.findById(req.params.id, (err, survey) => {
+      if (err) {
+        console.error(err);
+        res.end(err);
+      } else {
+        res.render('surveys/edit', {
+          title: survey.title,
+          description: survey.description,
+          survey: survey,
+          displayName: req.user ? req.user.displayName : ''
+        });
+      }
+    });
+  }
 };
 
 module.exports.editSurvey = (req, res, next) => {
+  if (!req.user) {
+    res.redirect('/login');
 
-  let id = req.params.id
+  }
+  else {
+    let id = req.params.id
 
-  let updatedSurvey = Survey({
-    _id: id,
-    title: req.body.title,
-    description: req.body.description
-  });
-  Survey.updateOne({ _id: id }, updatedSurvey, (err) => {
-    if (err) {
-      console.log(err);
-      res.end(err);
-    }
-    else {
-      // refresh the book list
-      res.redirect('/surveys');
-    }
-  });
-
-
+    let updatedSurvey = Survey({
+      _id: id,
+      title: req.body.title,
+      description: req.body.description
+    });
+    Survey.updateOne({ _id: id }, updatedSurvey, (err) => {
+      if (err) {
+        console.log(err);
+        res.end(err);
+      }
+      else {
+        // refresh the book list
+        res.redirect('/surveys');
+      }
+    });
+  }
 };
 
 module.exports.displaySurvey = (req, res, next) => {
@@ -101,17 +111,21 @@ module.exports.displaySurvey = (req, res, next) => {
 };
 
 module.exports.destroySurvey = (req, res, next) => {
-  let id = req.params.id;
-  Survey.remove({ _id: id }, (err) => {
-    if (err) {
-      console.log(err);
-      res.end(err);
-    }
-    else {
-      res.redirect('/surveys');
-    }
+  if (!req.user) {
+    res.redirect('/login');
 
-  });
+  }
+  else {
+    let id = req.params.id;
+    Survey.remove({ _id: id }, (err) => {
+      if (err) {
+        console.log(err);
+        res.end(err);
+      }
+      else {
+        res.redirect('/surveys');
+      }
 
-
+    });
+  }
 };
